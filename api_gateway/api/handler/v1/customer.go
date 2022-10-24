1a56ed82971f13f26d3cd897866e0b6a1a818b7c
RahimjonovMuhammadUmar <rahimzanovmuhammadumar@gmail.com>
2022-10-24 12:50:42 +0500

    New changes

--- api_gateway/api/handler/v1/customer.go
+++ api_gateway/api/handler/v1/customer.go
@@ -1,7 +1,56 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	pbc "exam/api_gateway/genproto/customer"
+	l "exam/api_gateway/pkg/logger"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+
+
+
+// Create creates customer
+// @Summary create customer api
+// @Description this api creates new customer
+// @Tags product
+// @Accept json
+// @Produce json
+// @Param customer body customer.CustomerRequest true "Customer"
+// @Success 201 {json} customer.Customer
+// @Router /v1/customer [post]
 func (h *handlerV1) CreateCustomer(c *gin.Context) {
-	
+	var (
+		body        pbc.CustomerRequest
+		jspbMarshal protojson.MarshalOptions
+	)
+
+	jspbMarshal.UseProtoNames = true
+
+	err := c.ShouldBindJSON(&body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		h.log.Error("failed to bind json", l.Error(err))
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(h.cfg.CtxTimeout))
+	defer cancel()
+
+	response, err := h.serviceManager.CustomerService().CreateCustomer(ctx, &body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		h.log.Error("error while declaring reponse api/hanlder/v1/customer.go", l.Error(err))
+		return
+	}
+	c.JSON(http.StatusCreated, response)
+
 }
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/swaggo/gin-swagger v1.5.3
 	go.uber.org/zap v1.23.0
 	google.golang.org/grpc v1.50.1
+	google.golang.org/protobuf v1.28.0
 )
 
 require (
@@ -45,6 +46,5 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.7 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
