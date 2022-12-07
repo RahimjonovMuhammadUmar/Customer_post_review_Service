f388fd77417bbed7af99bb536482d82d72e8b80a
RahimjonovMuhammadUmar <rahimzanovmuhammadumar@gmail.com>
2022-12-07 09:43:47 +0500

    Host

--- api_gateway/api/docs/docs.go
+++ api_gateway/api/docs/docs.go
@@ -1157,7 +1157,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "54.95.64.84:8800",
+	Host:             "localhost:8800",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Api's of all services",
